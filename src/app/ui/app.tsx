e2326cd0c3f00ecdf841a357cf5054a1120acd99
Ekaterina Tokarenko <kate.tokarenko8@gmail.com>
2024-11-17 17:21:18 +0200

    feat(styles): add reset css file, add index.scss

--- src/app/ui/app.tsx
+++ src/app/ui/app.tsx
@@ -1,11 +1,12 @@
+import '../styles/index.scss';
 import { Outlet } from "react-router-dom";
 import { Navbar } from "@/widgets/navbar";
 
 export const App = () => {
   return (
-    <>
+    <div className="app">
       <Navbar />
       <Outlet />
-    </>
+    </div>
   );
 };
