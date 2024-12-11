f897179e5d470b8d7ac8565351cf2de66c32c4b2
Ekaterina Tokarenko <kate.tokarenko8@gmail.com>
2024-12-11 21:08:43 +0200

    feat(styles): add responsive styles mixin, install react detect device package

--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -1,6 +1,8 @@
 import cls from "./navbar.m.scss";
-import { UiIcon } from "@/shared/ui/ui-icon/ui-icon";
 import Logo from "@/shared/assets/icons/logo-86-20.svg";
+import { UiIcon } from "@/shared/ui/ui-icon/ui-icon";
+import { UiHStack } from "@/shared/ui/ui-stack/ui-hstack/ui-hstack";
+import { UiBox } from "@/shared/ui/ui-box/ui-box";
 import { Menu } from "@/features/menu";
 import { Search } from "@/features/search";
 import { Login } from "@/features/login";
@@ -9,13 +11,15 @@ import { Cart } from "@/features/cart";
 export const Navbar = () => {
   return (
     <nav className={cls.navbar}>
-      <UiIcon Svg={Logo} />
-      <Menu />
-      <div className={cls.navbarGroup}>
-        <Search />
-        <Login />
-        <Cart />
-      </div>
+      <UiBox className={cls.navbarContent}>
+        <UiIcon Svg={Logo} />
+        <Menu />
+        <UiHStack className={cls.navbarGroup} max>
+          <Search />
+          <Login />
+          <Cart />
+        </UiHStack>
+      </UiBox>
     </nav>
   );
 };
