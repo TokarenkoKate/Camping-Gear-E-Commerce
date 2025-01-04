fc9900cd68ffcbd599afd5a7fb92da92364b447a
Ekaterina Tokarenko <kate.tokarenko8@gmail.com>
2025-01-04 17:31:43 +0200

    feat(testing): add inveted storybook example variant

--- src/shared/ui/storybook-example/storybook-example.tsx
+++ src/shared/ui/storybook-example/storybook-example.tsx
@@ -3,17 +3,23 @@ import { UiBox } from "../ui-box/ui-box";
 import { UiText } from "../ui-text/ui-text";
 import { UiVStack } from "../ui-stack/ui-vstack/ui-vstack";
 import cls from "./stotybook-example.m.scss";
+import classNames from "classnames";
 
 interface StorybookExampleProps extends PropsWithChildren {
   title: string;
+  inverted?: boolean;
 }
 
 export const StorybookExample = ({
   children,
   title,
+  inverted,
 }: StorybookExampleProps) => {
   return (
-    <UiVStack className={cls.storybookExample} max>
+    <UiVStack
+      className={classNames(cls.storybookExample, { [cls.inverted]: inverted })}
+      max
+    >
       <UiBox className={cls.storybookExampleHeader} max>
         <UiText as="h2" variant="headingXs" fontWeight="medium">
           {title}
