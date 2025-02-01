c16f877d91d23989e74ae8010c9728d94c7f04ff
Ekaterina Tokarenko <kate.tokarenko8@gmail.com>
2025-02-01 17:20:57 +0200

    feat(ui): add ProductsGallery to products page

--- src/pages/products/ui/products-page.tsx
+++ src/pages/products/ui/products-page.tsx
@@ -3,7 +3,9 @@ import { UiBox } from "@/shared/ui/ui-box/ui-box";
 import { EMPTY_STRING } from "@/shared/const/common-string";
 import { ProductsPageParams } from "../types/products-page";
 import { ProductsPageHeader } from "./products-page-header";
+import { ProductsGallery } from "@/features/products-gallery";
 import { categoryItems } from "@/entities/category/model/const/shop-categories";
+import { mockProductsGallery } from "@/mocks/products/product";
 import cls from "./products-page.m.scss";
 
 export const ProductsPage = () => {
@@ -17,6 +19,10 @@ export const ProductsPage = () => {
   return (
     <UiBox className={cls.productsPage}>
       <ProductsPageHeader categoryName={categoryName} />
+      <ProductsGallery
+        products={mockProductsGallery}
+        className={cls.productsPageGallery}
+      />
     </UiBox>
   );
 };
