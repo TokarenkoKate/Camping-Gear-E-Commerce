743e99e2e2ef8798d5523ea7b21f6cf8f42e3f49
Ekaterina Tokarenko <kate.tokarenko8@gmail.com>
2025-02-10 17:59:56 +0200

    feat(ui): create ProductSimilarItems section in ProductPage

--- src/pages/product/ui/product-page.tsx
+++ src/pages/product/ui/product-page.tsx
@@ -3,6 +3,7 @@ import { ProductInfo } from "./product-info/product-info";
 import { mockProductDetails } from "@/mocks/products/product";
 import { ProductVisuals } from "./product-visuals/product-visuals";
 import { ProductSpecifications } from "./product-specifications/product-specifications";
+import { ProductSimilarItems } from "./product-similar-items/product-similar-items";
 import cls from "./product-page.m.scss";
 
 export const ProductPage = () => {
@@ -13,6 +14,7 @@ export const ProductPage = () => {
       <ProductSpecifications
         specifications={mockProductDetails.specifications}
       />
+      <ProductSimilarItems />
     </UiBox>
   );
 };
